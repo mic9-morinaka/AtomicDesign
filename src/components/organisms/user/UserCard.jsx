f824ee85d92990896424b5ef7f86f400a014eee5
mic9-morinaka <67672299+mic9-morinaka@users.noreply.github.com>
2021-07-28 01:58:14 +0900

    ベース作成

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dt>11111@aaaaa.com</dt>
+        <dt>TEL</dt>
+        <dt>000-9999-8888</dt>
+        <dt>会社名</dt>
+        <dt>あああああああああ</dt>
+        <dt>WEB</dt>
+        <dt>dddddddd.com</dt>
+      </dl>
+    </div>
+  );
+};
