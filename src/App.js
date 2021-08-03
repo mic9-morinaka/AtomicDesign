d8c2a6388ff3f457a08502eb883d3350bd449b75
mic9-morinaka <67672299+mic9-morinaka@users.noreply.github.com>
2021-08-03 09:07:58 +0900

    grobalstateの受け渡し

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { Router } from "./router/Router";
+import { UserProvider } from "./providers/UserProvider";
 
 import "./styles.css";
 
 export default function App() {
-  return <Router />;
+  return (
+    <UserProvider>
+      <Router />
+    </UserProvider>
+  );
 }
